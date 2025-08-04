4178005df4f20fa2c4db479e3bf094af207c225d
NightWolfCZ <mirekrangl87@gmail.com>
2025-08-04 12:13:43 +0200

    edit

--- script/script.js
+++ script/script.js
@@ -6,3 +6,15 @@ function scrollToSection(id) {
     console.warn("Sekce '" + id + "' nebyla nalezena.");
   }
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+  if (window.netlifyIdentity) {
+    window.netlifyIdentity.on("init", user => {
+      if (!user) {
+        window.netlifyIdentity.on("login", () => {
+          document.location.href = "/admin/";
+        });
+      }
+    });
+  }
+});
